bb2ca5df55809fbb81c94ef059c2d88ab743c3a7
Luis Quintana <plissken2013es@gmail.com>
2014-09-11 15:33:07 +0200

    Added StartScene Main Title

--- src/StartupScene.js
+++ src/StartupScene.js
@@ -1,6 +1,8 @@
 function StartupScene(game) {
     this._game = game;
     
+    this._count = 0;
+    
     document.getElementById("divUI").style.display = "none";
 }
 
@@ -20,12 +22,27 @@ StartupScene.prototype._clearCanvas = function(ctx) {
 };
 
 StartupScene.prototype._drawText = function (ctx) {
-    ctx.fillStyle = "rgb(26, 26, 26)";
+    ctx.fillStyle = "rgba(26, 26, 26, " + Math.abs(Math.cos(this._count / Math.PI / 4)) +")";
     ctx.font = "bold 14px Verdana, Geneva, sans-serif";
     var text = "PRESS ENTER KEY OR TAP HERE";
     var textWidth = ctx.measureText(text).width;
     var x = ctx.canvas.width/2 - textWidth/2;
-    var y = ctx.canvas.height/2;
+    var y = ctx.canvas.height * 0.75;
+    ctx.fillText(text, x, y);
+    
+    ctx.fillStyle = "rgb(26, 26, 26)";
+    ctx.font = "bold 24px Verdana, Geneva, sans-serif";
+    text = "GODAI";
+    textWidth = ctx.measureText(text).width;
+    var x = ctx.canvas.width/2 - textWidth/2;
+    var y = ctx.canvas.height * 0.40;
+    ctx.fillText(text, x, y);
+    
+    ctx.font = "bold 21px Verdana, Geneva, sans-serif";
+    text = "The five great";
+    textWidth = ctx.measureText(text).width;
+    var x = ctx.canvas.width/2 - textWidth/2;
+    var y = ctx.canvas.height * 0.50;
     ctx.fillText(text, x, y);
 };
 
@@ -35,5 +52,5 @@ StartupScene.prototype.draw = function(ctx) {
 };
 
 StartupScene.prototype.tick = function() {
-    
+    this._count ++;
 };
